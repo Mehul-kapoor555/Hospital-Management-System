a147dacf0df39a9fcf9aab0b6e3c8ce7875ff18f
Mehul Kapoor <kapoormehul528@gmail.com>
2026-03-29 17:43:02 +0200

    Update database.php

--- src/database.php
+++ src/database.php
@@ -1,8 +1,8 @@
 <?php
     $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "mehul2003";
-    $db_name = "hospital";
+    $db_user = "username";
+    $db_pass = "password";
+    $db_name = "database_name";
     $conn = "";
 
     try{
